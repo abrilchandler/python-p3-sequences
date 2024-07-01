002fd90a7093f653a5bb1edb5a825d9a106fc666
Chandler Abril <abrilchandler1@gmail.com>
2024-07-01 07:32:19 -0700

    Done

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,18 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    if length == 0:
+        print("[]")
+        return
+    
+    first = 0
+    second = 1
+
+    fib_sequence = [first]
+
+    for _ in range(length - 1):
+        fib_sequence.append(second)
+        first, second = second, first + second
+
+    print(fib_sequence)
+print_fibonacci(9)
